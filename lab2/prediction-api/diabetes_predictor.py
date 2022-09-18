4840c5f0767da1470070189f58c547a90b9c342f
IndikaKuma <ipkumarawd@yahoo.com>
2022-09-18 17:07:02 +0200

    Add Docker files

---
+++ lab2/prediction-api/diabetes_predictor.py
@@ -0,0 +1,24 @@
+import json
+
+import pandas as pd
+from flask import jsonify
+from keras.models import load_model
+
+
+class DiabetesPredictor:
+    def __init__(self):
+        self.model = None
+
+    def predict_single_record(self, prediction_input):
+        print(prediction_input)
+        if self.model is None:
+            self.model = load_model('model.h5')
+        print(json.dumps(prediction_input))
+        df = pd.read_json(json.dumps(prediction_input), orient='records')
+        print(df)
+        y_pred = self.model.predict(df)
+        print(y_pred[0])
+        status = (y_pred[0] > 0.5)
+        print(type(status[0]))
+        # return the prediction outcome as a json message. 200 is HTTP status code 200, indicating successful completion
+        return jsonify({'result': str(status[0])}), 200
