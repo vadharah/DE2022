6173cdd490ea35949ef13387adc3af20977db047
IndikaKuma <indika.kuma@gmail.com>
2022-09-26 12:59:18 +0200

    Added answers to the lab exercise

--- lab2/prediction-api/diabetes_predictor.py
+++ lab2/prediction-api/diabetes_predictor.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 import pandas as pd
 from flask import jsonify
@@ -12,7 +13,12 @@ class DiabetesPredictor:
     def predict_single_record(self, prediction_input):
         print(prediction_input)
         if self.model is None:
-            self.model = load_model('model.h5')
+            model_repo = os.environ['MODEL_REPO']
+            if model_repo:
+                file_path = os.path.join(model_repo, "model.h5")
+                self.model = load_model(file_path)
+            else:
+                self.model = load_model('model.h5')
         print(json.dumps(prediction_input))
         df = pd.read_json(json.dumps(prediction_input), orient='records')
         print(df)
