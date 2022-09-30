81d582cee289182053db75713bdba0340748f11b
IndikaKuma <indika.kuma@gmail.com>
2022-09-30 14:27:36 +0200

    Update Lab3 exercise code

--- lab3/prediction-ui-embedded/diabetes_predictor.py
+++ lab3/prediction-ui-embedded/diabetes_predictor.py
@@ -12,16 +12,7 @@ class DiabetesPredictor:
 
     def predict_single_record(self, prediction_input):
         if self.model is None:
-            try:
-                model_repo = os.environ['MODEL_REPO']
-                if model_repo:
-                    file_path = os.path.join(model_repo, "model.h5")
-                    self.model = load_model(file_path)
-                else:
-                    self.model = load_model('model.h5')
-            except KeyError:
-                self.model = load_model('model.h5')
-
+            self.model = load_model('model.h5')
         df = pd.read_json(json.dumps(prediction_input), orient='records')
         y_pred = self.model.predict(df)
         print(y_pred[0])
