352ad6ceca60dd195946bc372702f6aa567531fb
IndikaKuma <ipkumarawd@yahoo.com>
2022-10-09 22:40:23 +0200

    Update test case in the live ab exercise

--- lab3/prediction-ui-embedded-gcs/tests/test_01_diabetes_predictor.py
+++ lab3/prediction-ui-embedded-gcs/tests/test_01_diabetes_predictor.py
@@ -1,4 +1,6 @@
 # content of test_sysexit.py
+import os
+import pytest
 import pandas as pd
 
 # content of test_class.py
@@ -6,6 +8,12 @@ import diabetes_predictor
 
 
 class TestDiabetesPredictor:
+
+    @pytest.fixture(scope="session", autouse=True)
+    def execute_before_any_test(self):
+        os.environ["MODEL_NAME"] = "testResources/model.h5"
+
+    # your setup code goes here, executed ahead of first test
     def test_predict_single_record(self):
         with open('testResources/prediction_request.json') as json_file:
             data = pd.read_json(json_file)
